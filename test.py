deac4bce9ca8a7667a02b2793179199019366bb8
vndee <vndee.huynh@gmail.com>
2022-07-06 09:16:45 +0700

    update

---
+++ test.py
@@ -0,0 +1,16 @@
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+article_en = "The head of the United Nations says there is no military solution in Syria"
+article_en = "The erosion of mosses on the surface of Hue imperial citadel has caused a lot of harm and is currently an urgent problem to be solved."
+model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-one-to-many-mmt")
+tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-one-to-many-mmt", src_lang="en_XX")
+
+model_inputs = tokenizer(article_en, return_tensors="pt")
+
+generated_tokens = model.generate(
+    **model_inputs,
+    forced_bos_token_id=tokenizer.lang_code_to_id["vi_VN"]
+)
+target = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+print(target)
+
